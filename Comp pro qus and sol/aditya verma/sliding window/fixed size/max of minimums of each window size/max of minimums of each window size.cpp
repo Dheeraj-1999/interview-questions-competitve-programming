fc306642ccf388af9a8af41a153ed177d0320e65
DNg <dheerajnagpal21081999@yahoo.com>
2020-10-17 22:20:24 +0530

    adding max of minimums

--- Comp pro qus and sol/aditya verma/sliding window/fixed size/max of minimums of each window size/max of minimums of each window size.cpp
+++ Comp pro qus and sol/aditya verma/sliding window/fixed size/max of minimums of each window size/max of minimums of each window size.cpp
@@ -1,9 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int> findMinsSizei(int* arr, int n, int k) {
+    vector<int> ans;
+
+    deque<int> dq(k);
+
+}
+
+void maxOfMins(int* arr, int n) 0{
+    vector<int> minArr;
+    for(int i=1; i<=n; i++) {
+        minArr = findMinsSizei(arr, n, i);
+    }
+    int maxMinArr = INT_MIN;
+    for(int i=0; i<minArr.size(); i++ ) {
+        maxMinArr = max(minArr[i], maxMinArr);
+    }
+    cout << maxMinArr << " ";
+}
+
 int main(int argc, char const *argv[])
 {
     
-    
+    int n;
+    cin >> n;
+
+    int* arr = new int[n];
+    for(int i=0; i<n; i++)
+        cin >>  arr[i];
     return 0;
 }
