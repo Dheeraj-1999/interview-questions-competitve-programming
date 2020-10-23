3f158a5eda2755d29579399b0a1092a77bb94159
DNg <dheerajnagpal21081999@yahoo.com>
2020-10-23 12:40:20 +0530

    adding strievers sde question bank

--- Comp pro qus and sol/aditya verma/sliding window/fixed size/max of minimums of each window size/max of minimums of each window size.cpp
+++ Comp pro qus and sol/aditya verma/sliding window/fixed size/max of minimums of each window size/max of minimums of each window size.cpp
@@ -4,20 +4,56 @@ using namespace std;
 vector<int> findMinsSizei(int* arr, int n, int k) {
     vector<int> ans;
 
-    deque<int> dq(k);
+    // deque<int> dq(k);
+    deque<int> q;
+    //  cout <<"he " << q.front() << " ";  
+    
+    // =============== for first k elements ==================
+    for(int i=0; i<k; i++) {
+        while( (!q.empty()) and arr[q.back()] >= arr[i] ) {
+            q.pop_back();
+        }
+        q.push_back(i);
+        // cout << q.front() << " ";
+    }
+
+
+//  =================== for next K element ================
+    // cout << q.front() << " ";
+    for(int i=k; i<n; i++) {
+        cout << arr[q.front()] << " ";
+
+        //conditon for : keep only those values which are part of window size
+        while(q.front() <= (i-k) and !(q.empty()) ) { 
+           q.pop_front();
+        }
+        while((!q.empty()) and arr[q.back()] >= arr[i] ) {
+            q.pop_back();
+        }
+        q.push_back(i);
+    }
+    ans.push_back(arr[q.front()] );
+
 
 }
 
-void maxOfMins(int* arr, int n) 0{
-    vector<int> minArr;
+void maxOfMins(int* arr, int n) {
+    
     for(int i=1; i<=n; i++) {
+        vector<int> minArr;
         minArr = findMinsSizei(arr, n, i);
+        // int maxMinArr = INT_MIN;
+        // for(int i=0; i<minArr.size(); i++ ) {
+        //     maxMinArr = max(minArr[i], maxMinArr);
+        // }
+        // cout << maxMinArr << " ";
+        for(int i=0; i<minArr.size(); i++ ) {
+            // maxMinArr = max(minArr[i], maxMinArr);
+            cout << minArr[i] << " ";
+        }
+        cout << endl;
     }
-    int maxMinArr = INT_MIN;
-    for(int i=0; i<minArr.size(); i++ ) {
-        maxMinArr = max(minArr[i], maxMinArr);
-    }
-    cout << maxMinArr << " ";
+    
 }
 
 int main(int argc, char const *argv[])
@@ -29,5 +65,6 @@ int main(int argc, char const *argv[])
     int* arr = new int[n];
     for(int i=0; i<n; i++)
         cin >>  arr[i];
+    maxOfMins(arr, n);
     return 0;
 }
