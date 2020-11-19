f4a3fed94ff42c7ef8deb3d3f7ac854fc14a1c7a
DNg <dheerajnagpal21081999@yahoo.com>
2020-11-19 23:21:09 +0530

    some changes in subset sum with mini diff

--- Comp pro qus and sol/gfg/placement series/dp/1.4 minimum subset sum diff.cpp
+++ Comp pro qus and sol/gfg/placement series/dp/1.4 minimum subset sum diff.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 // static int counts = 0;
+
+/*
 int mini =INT_MAX;
 int minSubsetDiff(int* arr, int n) {
     if(n == 0) {
@@ -49,6 +51,65 @@ int minSubsetDiffDp(int* arr, int n, int sum) {
     }
     return dp[n][sum];
 }
+*/
+
+
+int** subsetSumDp(int* arr, int sum, int n) {
+    int** dp = new int* [n+1]; //size sum x n
+    for(int i=0; i<n+1; i++) {
+        dp[i] = new int[sum+1];
+        for(int j=0; j<n; j++) {
+            dp[i][j] = -1;
+            if( i == 0) dp[i][j] = 0;
+            if(j == 0) dp[i][j] = 1;
+        }
+    }
+
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=sum; j++) {
+            if(arr[i-1] <= j) {
+                int opt1 = dp[i-1][j-arr[i-1]]; //include the number
+                int opt2 = dp[i-1][j]; // exclude the number
+                dp[i][j] = opt1 or opt2;
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }   
+        }
+    }
+
+    // for(int i=0; i<n; i++) {
+    //     for(int j=0; j<sum; j++) {
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    return dp;
+}
+
+void minSubsetDiffDp(int* arr, int n, int sum) {
+    // int dp[n+1][sum+1];
+    int** dp = new int* [n+1]; //size sum x n
+    for(int i=0; i<n+1; i++) {
+        dp[i] = new int[sum+1];
+        for(int j=0; j<n; j++) {
+            dp[i][j] = -1;
+        }
+    }
+
+    dp = subsetSumDp(arr, sum, n);
+
+    // memset(dp, -1, sizeof(dp));
+    vector<int> minsVector;
+    for(int i=0; i<sum/2; i++) {
+        if(dp[n][i]) {
+            minsVector.push_back(i);
+        }
+    }
+    for(int i=0; i<minsVector.size(); i++) {
+        cout << minsVector[i] << " ";
+    }
+}
 
 int main(int argc, char const *argv[])
 {
@@ -59,6 +120,6 @@ int main(int argc, char const *argv[])
         cin >> set[i]; 
         sumTotal += set[i];
     }
-    cout << minSubsetDiff2(set, n, sumTotal, sumTotal);   
+    minSubsetDiffDp(set, n, sumTotal);   
     return 0;
 }
